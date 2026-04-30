98e18f4b94c6e63a53340594db6d57341b15dfbb
Olga Sova <sova_277@mail.ru>
2026-04-30 18:29:41 +0400

    Создан новый класс SleepingSession

---
+++ src/main/java/ru/yandex/practicum/sleeptracker/SleepingSession.java
@@ -0,0 +1,63 @@
+package ru.yandex.practicum.sleeptracker;
+
+public class SleepingSession {
+    private String date; // Дата сна
+    private int startTime; // Время начала сна (например, в минутах от начала суток)
+    private int endTime; // Время окончания сна
+    private boolean isRestful; // Флаг, указывающий на качество сна
+
+    public SleepingSession(String date, int startTime, int endTime, boolean isRestful) {
+        this.date = date;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.isRestful = isRestful;
+    }
+
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public int getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(int startTime) {
+        this.startTime = startTime;
+    }
+
+    public int getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(int endTime) {
+        this.endTime = endTime;
+    }
+
+    public boolean isRestful() {
+        return isRestful;
+    }
+
+    public void setRestful(boolean restful) {
+        isRestful = restful;
+    }
+
+    @Override
+    public String toString() {
+        return "SleepingSession{" +
+                "date='" + date + '\'' +
+                ", startTime=" + startTime +
+                ", endTime=" + endTime +
+                ", isRestful=" + isRestful +
+                '}';
+    }
+
+    // Дополнительный метод — расчёт продолжительности сна в минутах
+    public int getDuration() {
+        return endTime - startTime;
+    }
+}
