7aa156c941ff5c16b577ce7ac00239af831677bc
Olga Sova <sova_277@mail.ru>
2026-05-10 10:32:59 +0400

    В NightSleepTracker добавлен подсчёт бессонных ночей во временном периоде

--- src/main/java/ru/yandex/practicum/sleeptracker/NightSleepTracker.java
+++ src/main/java/ru/yandex/practicum/sleeptracker/NightSleepTracker.java
@@ -1,7 +1,12 @@
 package ru.yandex.practicum.sleeptracker;
 
+import java.time.LocalDate;
 import java.time.LocalTime;
 import java.time.LocalDateTime;
+import java.time.Period;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.LongStream;
 
 public class NightSleepTracker {
     LocalTime nightStart = LocalTime.of(22, 0);
@@ -14,4 +19,24 @@ public class NightSleepTracker {
         return (startTime.toLocalTime().isBefore(nightEnd) || startTime.toLocalTime().equals(nightEnd))
                 && (endTime.toLocalTime().isAfter(nightStart) || endTime.toLocalTime().equals(nightStart));
     }
+
+
+    public static long countDaysInPeriod(LocalDate startDate, LocalDate endDate) {
+        Period basePeriod = Period.between(startDate, endDate);
+
+        return basePeriod.getDays() + 1;
+    }
+
+    public List<LocalDate> getSleeplessNights(LocalDate startDate, LocalDate endDate, List<SleepingSession> sessions) {
+
+        long totalDays = countDaysInPeriod(startDate, endDate);
+
+        return LongStream.range(0, totalDays)
+                .mapToObj(startDate::plusDays)
+                .filter(date -> sessions.stream()
+                        .noneMatch(session -> isSessionDuringNight(session)
+                                && session.getStartTime().toLocalDate().equals(date)))
+                .collect(Collectors.toList());
+    }
 }
+
